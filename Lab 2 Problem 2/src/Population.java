f10013a7d9a1e36e600a5f11144e37aa02d11093
tgnass <tgnass@gmail.com>
2015-09-22 10:58:16 -0400

    Small edits

--- Lab 2 Problem 2/src/Population.java
+++ Lab 2 Problem 2/src/Population.java
@@ -1,15 +1,9 @@
-
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author tgnas_000
+ * @author Tyler
  */
 public class Population {
 
